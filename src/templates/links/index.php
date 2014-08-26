3041275e373830b5e78349a5b5db45c50a9186a6
Jeff Finley <quickliketurtle@gmail.com>
2014-08-25 22:12:33 -0700

    Moved error and success message to same place, styled to match.

--- src/templates/links/index.php
+++ src/templates/links/index.php
@@ -10,10 +10,7 @@
     <div class="container put-the-dang-thing-in-the-middle">
         <div class="col-md-2"></div>
         <div class="col-md-8">
-            <div class="alert alert-dismissable alert-danger error hidden">
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-                <?php if (isset($flash['error'])) { echo $flash['error']; } ?>
-            </div>
+
             <h1 class="">Shorten A URL</h1>
             <div class="form">
                 <form role="form" action="links" method="post">
@@ -25,10 +22,15 @@
                     </div>
                 </form>
             </div>
-            <div class="alert" style="text-align: center">
+            <div class="alert alert-dismissable alert-danger error hidden">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?php if (isset($flash['error'])) { echo $flash['error']; } ?>
+            </div>
+            <div class="alert alert-dismissable alert-info message hidden" style="text-align: center">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
                 <?php if (isset($flash['flash_message']))
-                    { echo "Here is your short URL: <a href=" . $flash['flash_message'] .
-                        ">" . $flash['flash_message'] . "</a>";
+                { echo "Here is your short URL: <a href=" . $flash['flash_message'] . ">" .
+                    $flash['flash_message'] . "</a>";
                 } ?>
             </div>
         </div>
@@ -37,10 +39,17 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript" src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script>
-        // display error alert is error's exist.
+        // display error alert if error exist.
         var error = "<?php echo $flash['error']; ?>";
         if (error) {
             $('.error').removeClass('hidden');
         }
+
+        // display shortened url.
+        var message = "<?php echo $flash['flash_message']; ?>";
+        if (message) {
+            $('.message').removeClass('hidden');
+        }
+
     </script>
 </body>
